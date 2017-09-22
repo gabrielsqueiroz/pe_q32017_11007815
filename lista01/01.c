036666f4badcc1b113c57cf05b19de8ca188115a
Convidado <gabrielsqueiroz>
2017-09-22 12:05:52 -0300

    Exercício 01 da lista01

---
+++ lista01/01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+	int valor;
+	printf ("Entre com um valor\n") ;
+	scanf ("%d", &valor) ;
+
+	if (valor%2==0){
+		printf ("Numero é par\n");
+	}
+	else{
+		printf ("Numero é ímpar\n");
+	}
+	return 0;
+}
